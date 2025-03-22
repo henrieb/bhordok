6b025a1bfa749df5e880d1be304ede741beea584
Pedro Henrique Simoncini <ph.simoncini@gmail.com>
2025-03-22 13:54:15 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
-print("Olá mundo!")
+import discord
+from discord import app_commands
+class rpgshop(discord.Client):
+    def __init__(self):
+        intents = discord.Intents.all()
+        super().__init__(
+            command_prefix="$",
+            intents=intents
+        )
+        self.tree = app_commands.CommandTree(self)
+    async def setup_hook(self):
+        await self.tree.sync()
+    async def on_ready(self):
+        print(f"O bot {self.user} está funcionando.")
+
+bot = rpgshop()
+
+
+@bot.tree.command(name="olá-mundo", description="Primeiro comando do bot")
+async def olamundo(interaction:discord.Interaction): 
+    await interaction.response.send_message(f"Olá, {interaction.user.mention}!")
+
+@bot.tree.command(name="soma", description="Comando de soma")
+@app_commands.describe(
+    numero1="Primeiro número",
+    numero2="Segundo número" 
+)
+async def soma(interaction:discord.Interaction, numero1:int,numero2:int):
+    numero_somado = numero1 + numero2
+    await interaction.response.send_message(f"Olá, {interaction.user.mention}! O resultado da sua soma é {numero_somado}")
+
+bot.run("MTM0MDg2NzcxNjM3NDA3MzQwNg.G3w1pC.3ug00S4nGSqkikpWE_FIoioREIFHWMwNjTJ00s")
